243334924c966d4dbed3972eb01f67cf1b133b90
limboden <imbodenlu@gmail.com>
2024-12-05 16:56:40 -0500

    clean slate for resolvers

--- server/src/schemas/resolvers.ts
+++ server/src/schemas/resolvers.ts
@@ -1,32 +1,14 @@
-import { GraphQLContexts } from '../types/express/types'
+import { GraphQLContexts } from '../types/types'
+import User from '../models/User';
+import { signToken } from '../services/auth';
+import mongoose from 'mongoose';
 
 
-const books = [];
 
 const resolvers = {
   Query: {
-    books: () => books,
-    book: (parent, { id }) => books.find(book => book.id === id),
   },
   Mutation: {
-    addBook: (parent, { title, author }) => {
-      const newBook = { id: `${books.length + 1}`, title, author };
-      books.push(newBook);
-      return newBook;
-    },
-    updateBook: (parent, { id, title, author }) => {
-      const book = books.find(book => book.id === id);
-      if (!book) return null;
-      if (title) book.title = title;
-      if (author) book.author = author;
-      return book;
-    },
-    deleteBook: (parent, { id }) => {
-      const index = books.findIndex(book => book.id === id);
-      if (index === -1) return null;
-      const deletedBook = books.splice(index, 1);
-      return deletedBook[0];
-    },
   },
 };
 
